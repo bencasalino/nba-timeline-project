dc059b1822da0101e1296307217bf8c925add2e4
Ben Casalino <bencasalino@gmail.com>
2018-11-10 07:03:08 -0700

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,87 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="main">
+<div class="team-wrapper">
+      <div class="team" id="atl">
+        TEAM NAME
+      </div>
+    </div>
+    <div class="player-wrapper">
+      <div class="player">PLAYER</div>
+      <div class="player">PLAYER</div>
+      <div class="player">PLAYER</div>
+      <div class="player">PLAYER</div>
+      <div class="player">PLAYER</div>
+    </div>
+    <div class="player-image-wrapper">
+      <div class="player-image">
+        <a href="#">
+          <img src="#" alt="#">
+        </a>
+      </div>
+      <div class="player-image">
+        <a href="#">
+          <img src="#" alt="#">
+        </a>
+      </div>
+      <div class="player-image">
+        <a href="#">
+          <img src="#" alt="#">
+        </a>
+      </div>
+      <div class="player-image">
+        <a href="#">
+          <img src="#" alt="#">
+        </a>
+      </div>
+      <div class="player-image">
+        <a href="#">
+          <img src="#" alt="#">
+        </a>
+      </div>
+    </div>
+    </div>
+    </div>
 </template>
+
+<style scoped>
+.team-wrapper {
+  display: flex;
+}
+
+.team {
+  background-color: #666666;
+  padding: 10px;
+  max-width: 500px;
+  width: 500px;
+  text-align: center;
+  border-bottom: solid 2px;
+}
+
+.player-wrapper {
+  display: flex;
+}
+
+.player {
+  max-width: 100px;
+  width: 100px;
+  font-size: 12px;
+  text-align: center;
+  /* background-color: #555555; */
+  padding: 5px 2px 0 2px;
+  height: 20px;
+}
+
+.player-image-wrapper {
+  display: flex;
+}
+
+.player-image {
+  max-width: 100px;
+  width: 100px;
+  font-size: 12px;
+  text-align: center;
+  /* background-color: #444444; */
+  /* padding: 5px 2px 0 2px; */
+  height: 60px;
+}
+</style>
