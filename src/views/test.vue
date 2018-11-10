d32cdbb0fe52e4da442fa29183242083c1559ae4
Ben Casalino <bencasalino@gmail.com>
2018-11-10 07:54:33 -0700

    var error

---
+++ src/views/test.vue
@@ -0,0 +1,162 @@
+<template>
+
+</template>
+
+
+<style>
+/* EAST */
+
+  /* ATL */
+  --atl-bg-color: #e03a3e;
+  --atl-txt-color: #26282a;
+  --atl-alt-color: #c1d32f;
+  /* CHA */
+  /* --atl-bg-color: #1d1160; */
+  /* --atl-txt-color: #a1a1a4; */
+  /* --atl-alt-color: #00788c; */
+  /* ORL */
+  /* --atl-bg-color: #0077c0; */
+  /* --atl-txt-color: #c4ced4; */
+  /* --atl-alt-color: #000000; */
+  /* MIA */
+  /* --atl-bg-color: #98002e; */
+  /* --atl-txt-color: #f9a01b; */
+  /* --atl-alt-color: #000000; */
+  /* WAS */
+  /* --atl-bg-color: #002b5c; */
+  /* --atl-txt-color: #e31837; */
+  /* --atl-alt-color: #c4ced4; */
+  /* NYK */
+  /* --atl-bg-color: #006bb6;
+  --atl-txt-color: #f58426;
+  --atl-alt-color: #bec0c2; */
+  /* BKN */
+  /* --atl-bg-color: #000000;
+  --atl-txt-color: #ffffff;
+  --atl-alt-color: #777d84; */
+  /* TOR */
+  /* --atl-bg-color: #ce1141;
+  --atl-txt-color: #1A1A41;
+  --atl-alt-color: #000000; */
+  /* BOS */
+  /* --atl-bg-color: #007a33;
+  --atl-txt-color: #ba9653;
+  --atl-alt-color: #000000; */
+  /* PHI */
+  /* --atl-bg-color: #006bb6;
+  --atl-txt-color: #ed174c;
+  --atl-alt-color: #002b5c; */
+  /* #C4CED4 */
+  /* CHI */
+  /* --atl-bg-color: #ce1141;
+  --atl-txt-color: #ffffff;
+  --atl-alt-color: #000000; */
+  /* DET */
+  /* --atl-bg-color: #c8102e;
+  --atl-txt-color: #006bb6;
+  --atl-alt-color: #bec0c2; */
+  /* #002D62; */
+  /* MIL */
+  /* --atl-bg-color: #00471b;
+  --atl-txt-color: #eee1c6;
+  --atl-alt-color: #0077c0; */
+  /* IND */
+  /* --atl-bg-color: #002d62;
+  --atl-txt-color: #fdbb30;
+  --atl-alt-color: #bec0c2; */
+  /* CLE */
+  /* --atl-bg-color: #6f263d;
+  --atl-txt-color: #ffb81c;
+  --atl-alt-color: #041e42; */
+
+  /* WEST */
+  /* PHX */
+  /* --atl-bg-color: #1d1160;
+  --atl-txt-color: #e56020;
+  --atl-alt-color: #f9ad1b; */
+  /* 63727A */
+  /* LAL */
+  /* --atl-bg-color: #552583;
+  --atl-txt-color: #fdb927;
+  --atl-alt-color: #000000; */
+  /* LAC */
+  /* --atl-bg-color: #c8102e;
+  --atl-txt-color: #1d42ba;
+  --atl-alt-color: #bec0c2; */
+  /* SAC */
+  /* --atl-bg-color: #5a2d81;
+  --atl-txt-color: #63727a;
+  --atl-alt-color: #000000; */
+  /* GSW */
+  /* --atl-bg-color: #006bb6;
+  --atl-txt-color: #fdb927;
+  --atl-alt-color: #26282a; */
+  /* UTH */
+  /* --atl-bg-color: #002b5c; */
+  /* --atl-txt-color: #00471b; */
+  /* --atl-alt-color: #f9a01b; */
+  /* DEN */
+  /* --atl-bg-color: #0e2240; */
+  /* --atl-txt-color: #fec524; */
+  /* --atl-alt-color: #8b2131; */
+  /* #1D428A; */
+  /* OKC */
+  /* --atl-bg-color: #007ac1; */
+  /* --atl-txt-color: #ef3b24; */
+  /* --atl-alt-color: #fdbb30; */
+  /* #002D62; */
+  /* POR */
+  /* --atl-bg-color: #e03a3e; */
+  /* --atl-txt-color: #000000; */
+  /* --atl-alt-color: #666666; */
+  /* MIN */
+  /* --atl-bg-color: #0c2340; */
+  /* --atl-txt-color: #236192; */
+  /* --atl-alt-color: #78be20; */
+  /* SAS */
+  /* --atl-bg-color: #c4ced4;
+  --atl-txt-color: #000000;
+  --atl-alt-color: #8a8d8f; */
+  /* DAL */
+  /* --atl-bg-color: #00538c;
+  --atl-txt-color: #b8c4ca;
+  --atl-alt-color: #002b5e; */
+  /* #00843D; */
+  /* HOU */
+  /* --atl-bg-color: #ce1141;
+  --atl-txt-color: #ffc72c;
+  --atl-alt-color: #c4ced4; */
+  /* NOP */
+  /* --atl-bg-color: #0c2340;
+  --atl-txt-color: #c8102e;
+  --atl-alt-color: #85714d; */
+  /* MEP */
+  /* --atl-bg-color: #5d76a9;
+  --atl-txt-color: #12173f;
+  --atl-alt-color: #f5b112; */
+}
+
+/* WEST */
+
+/* PHX */
+/* LAL */
+/* LAC */
+/* SAC */
+/* GSW */
+
+/* UTH */
+/* DEN */
+/* OKC */
+/* POR */
+/* MIN */
+
+/* SAS */
+/* DAL */
+/* HOU */
+/* NOP */
+/* MEP */
+
+/* EAST */
+
+/* ATL */
+</style>
