53420c69bab2001588cf0e9461df945d7d2d9070
Ben Casalino <bencasalino@gmail.com>
2018-11-10 09:35:18 -0700

    add sass colors

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,46 +2,83 @@
   <div id="main">
     <div class="team-wrapper">
       <div class="team" id="atl">
-      ATL
+        ATL
       </div>
       <div class="player-wrapper">
         <div class="player">T. YOUNG</div>
-        <div class="player">K. BAZEMORE</div>
+        <div class="player alt">K. BAZEMORE</div>
         <div class="player">T. PRINCE</div>
-        <div class="player">O. SPELLMAN</div>
+        <div class="player alt">O. SPELLMAN</div>
         <div class="player">A. LEN</div>
       </div>
     </div>
-  </div>
 
+    <div class="team-wrapper">
+      <div class="team" id="orl">
+        ORL
+      </div>
+      <div class="player-wrapper">
+        <div class="player">D. AUGUSTIN</div>
+        <div class="player alt">E. FORUNIER</div>
+        <div class="player">J. ISSAC</div>
+        <div class="player alt">A. GORDON</div>
+        <div class="player">N. VUCEVIC</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
-<style scoped>
-:root {
-  --atl-bg-color: #e03a3e;
-  --atl-txt-color: #c1d32f;
-  --atl-alt-color: #26282a;
+<style lang="scss" scoped>
+@import "../scss/colors.scss";
+
+.team {
+  width: 50px;
+  max-width: 50px;
+  text-align: center;
+  font-weight: bold;
 }
-.team-wrapper > #atl {
-  background-color: var(--atl-bg-color);
-  color: var(--atl-txt-color);
-  border-color: var(--atl-alt-color);
+
+.team-wrapper,
+.player-wrapper {
+  display: flex;
+  flex-direction: row;
 }
 
 .team-wrapper {
-  display: flex;
+  padding-bottom: 20px;
 }
-.player-wrapper {
-  display: flex;
-}
+
 .player {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.player.alt {
+  background-color: #dcdcdc;
+}
+
+.player {
+  background-color: #d3d3d3;
   max-width: 100px;
   width: 100px;
   font-size: 12px;
   text-align: center;
-  /* background-color: #555555; */
-  padding: 5px 2px 0 2px;
-  height: 20px;
+  // padding: 5px 2px 0 2px;
+  // height: 20px;
+}
+
+#atl {
+  color: $atl-3;
+  background-color: $atl-1;
+  // border-bottom: 2px solid $atl-1;
+}
+#orl {
+  color: $orl-2;
+  background-color: $orl-1;
+  // border-bottom: 2px solid $orl-3;
 }
 </style>
+
+
+
--- src/views/test.vue
+++ src/views/test.vue
@@ -160,3 +160,34 @@
 
 /* ATL */
 </style>
+
+
+
+
+<style scoped>
+:root {
+  --atl-bg-color: #e03a3e;
+  --atl-txt-color: #c1d32f;
+  --atl-alt-color: #26282a;
+}
+.team-wrapper > #atl {
+  background-color: var(--atl-bg-color);
+  color: var(--atl-txt-color);
+  border-color: var(--atl-alt-color);
+}
+
+.team-wrapper {
+  display: flex;
+}
+.player-wrapper {
+  display: flex;
+}
+.player {
+  max-width: 100px;
+  width: 100px;
+  font-size: 12px;
+  text-align: center;
+  padding: 5px 2px 0 2px;
+  height: 20px;
+}
+</style>
