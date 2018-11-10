1712a11ba5c9ffd92f2bee8b337d71966171f078
Ben Casalino <bencasalino@gmail.com>
2018-11-10 10:04:49 -0700

    flex

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="main">
+    <div class="east">
+    <!-- // SOUTHEAST -->
     <div class="team-wrapper">
-      <div class="team" id="atl">
+      <div class="team" id="ATL">
         ATL
       </div>
       <div class="player-wrapper">
@@ -14,7 +16,7 @@
     </div>
 
     <div class="team-wrapper">
-      <div class="team" id="orl">
+      <div class="team" id="ORL">
         ORL
       </div>
       <div class="player-wrapper">
@@ -25,6 +27,379 @@
         <div class="player">N. VUCEVIC</div>
       </div>
     </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="MIA">
+        MIA
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="CHA">
+        CHA
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="WAS">
+        WAS
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+    <!-- // ATLANTIC -->
+
+    <div class="team-wrapper">
+      <div class="team" id="PHI">
+        PHI
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="TOR">
+        TOR
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="BOS">
+        BOS
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="nyk">
+        NYK
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="BKN">
+        BKN
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <!-- // CENTRAL -->
+
+    <div class="team-wrapper">
+      <div class="team" id="MIL">
+        MIL
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="DET">
+        DET
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="IND">
+        IND
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="CLE">
+        CLE
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <div class="team-wrapper">
+      <div class="team" id="CHI">
+        CHI
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+    </div>
+    <div class="west">
+
+    <!-- // NORTHWEST -->
+        <div class="team-wrapper">
+      <div class="team" id="POR">
+        POR
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+        <div class="team-wrapper">
+      <div class="team" id="DEN">
+        DEN
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+        <div class="team-wrapper">
+      <div class="team" id="OKC">
+        OKC
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+        <div class="team-wrapper">
+      <div class="team" id="MIN">
+        MIN
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+        <div class="team-wrapper">
+      <div class="team" id="UTH">
+        UTH
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+    <!-- //PACIFIC -->
+            <div class="team-wrapper">
+      <div class="team" id="LAC">
+        LAC
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="GSW">
+        GSW
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="SAC">
+        SAC
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="LAL">
+        LAL
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="PHX">
+        PHX
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+  <!-- // SOUTHWEST -->
+          <div class="team-wrapper">
+      <div class="team" id="MEP">
+        MEP
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="DAL">
+        DAL
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="NOP">
+        NOP
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="HOU">
+        HOU
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+
+            <div class="team-wrapper">
+      <div class="team" id="SAS">
+        SAS
+      </div>
+      <div class="player-wrapper">
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+        <div class="player alt">TEST</div>
+        <div class="player">TEST</div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -32,6 +407,14 @@
 <style lang="scss" scoped>
 @import "../scss/colors.scss";
 
+#main {
+  display: flex;
+  justify-content: space-around;
+}
+.east,
+.west {
+  // padding: 0 10px 0 10px;
+}
 .team {
   width: 50px;
   max-width: 50px;
@@ -46,7 +429,7 @@
 }
 
 .team-wrapper {
-  padding-bottom: 20px;
+  padding-bottom: 15px;
 }
 
 .player {
@@ -54,6 +437,7 @@
   justify-content: center;
   align-items: center;
 }
+
 .player.alt {
   background-color: #dcdcdc;
 }
@@ -68,15 +452,145 @@
   // height: 20px;
 }
 
-#atl {
-  color: $atl-3;
-  background-color: $atl-1;
-  // border-bottom: 2px solid $atl-1;
+// SOUTHEAST
+#ATL {
+  color: $ATL-3;
+  background-color: $ATL-1;
 }
-#orl {
-  color: $orl-2;
-  background-color: $orl-1;
-  // border-bottom: 2px solid $orl-3;
+
+#ORL {
+  color: $ORL-2;
+  background-color: $ORL-1;
+}
+
+#MIA {
+  color: $MIA-2;
+  background-color: $MIA-1;
+}
+
+#CHA {
+  color: $CHA-1;
+  background-color: $CHA-3;
+}
+
+#WAS {
+  color: $WAS-2;
+  background-color: $WAS-1;
+}
+
+// ATLANTIC
+#BOS {
+  color: $BOS-2;
+  background-color: $BOS-1;
+}
+
+#PHI {
+  color: $PHI-2;
+  background-color: $PHI-1;
+}
+
+#nyk {
+  color: $nyk-2;
+  background-color: $nyk-1;
+}
+
+#TOR {
+  color: $TOR-2;
+  background-color: $TOR-1;
+}
+
+#BKN {
+  color: $BKN-2;
+  background-color: $BKN-1;
+}
+
+// CENTRAL
+#MIL {
+  color: $MIL-2;
+  background-color: $MIL-1;
+}
+
+#CHI {
+  color: $CHI-2;
+  background-color: $CHI-1;
+}
+
+#DET {
+  color: $DET-1;
+  background-color: $DET-2;
+}
+
+#IND {
+  color: $IND-2;
+  background-color: $IND-1;
+}
+
+#CLE {
+  color: $CLE-2;
+  background-color: $CLE-1;
+}
+// PACIFIC
+#SAC {
+  color: $SAC-2;
+  background-color: $SAC-1;
+}
+#GSW {
+  color: $GSW-2;
+  background-color: $GSW-1;
+}
+#PHX {
+  color: $PHX-2;
+  background-color: $PHX-1;
+}
+#LAC {
+  color: $LAC-2;
+  background-color: $LAC-1;
+}
+#LAL {
+  color: $LAL-1;
+  background-color: $LAL-2;
+}
+// SOUTHWEST
+#NOP {
+  color: $NOP-1;
+  background-color: $NOP-3;
+}
+#MEP {
+  color: $MEP-2;
+  background-color: $MEP-1;
+}
+#DAL {
+  color: $DAL-2;
+  background-color: $DAL-1;
+}
+#HOU {
+  color: $HOU-2;
+  background-color: $HOU-1;
+}
+#SAS {
+  color: $SAS-2;
+  background-color: $SAS-1;
+}
+//NORTHWEST
+#POR {
+  color: $POR-3;
+  background-color: $POR-1;
+}
+#DEN {
+  color: $DEN-2;
+  background-color: $DEN-1;
+}
+#UTH {
+  color: $UTH-3;
+  background-color: $UTH-2;
+}
+#OKC {
+  color: $OKC-2;
+  background-color: $OKC-1;
+}
+#MIN {
+  color: $MIN-3;
+  background-color: $MIN-1;
 }
 </style>
 
